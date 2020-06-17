5c189db351bc6f48fa351445c5243f58d446cecf
Amon.Sabul <Amon.Sabul@BREFTLPT0012.CRAFTSILICON.LOCAL>
2020-06-17 17:16:47 +0300

    delete browser passwords from history made a bot

--- src/browserpass/BrowserPass.java
+++ src/browserpass/BrowserPass.java
@@ -1,44 +1,98 @@
 package browserpass;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 public class BrowserPass {
 
-    public static void main(String[] args) {
-        File folder = new File("Google\\Chrome\\User Data\\Default\\Web Data");
-        BrowserPass listFiles = new BrowserPass();
-        listFiles.listAllFiles(folder);
-        System.out.println("-------------------------------------------------");
+    public static void main(String[] args) { 
+
+        BrowserPass pass = new BrowserPass();
+        pass.chrome();
     }
 
-    public void listAllFiles(File folder) {
-        File[] fileNames = folder.listFiles();
-        for (File file : fileNames) {
-            if (file.isDirectory()) {
-                listAllFiles(file);
-            } else {
-                try {
-                    readContent(file);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    public void chrome() {
+        System.setProperty("webdriver.chrome.driver", "src\\browserpass\\chromedriver_2.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.linkedin.com/login");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.xpath("//button[text()='Sign in']"));
+        username.sendKeys("chepseron@gmail.com");
+        password.sendKeys("benson32!");
+        login.click();
+        String actualUrl = "https://www.linkedin.com/feed/";
+        String expectedUrl = driver.getCurrentUrl();
+        if (actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
         }
     }
 
-    public void readContent(File file) throws IOException {
-     //   System.out.println("read file " + file.getCanonicalPath());
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String strLine;
-            while ((strLine = br.readLine()) != null) {
-                if (strLine.toLowerCase().contains("password")) {
-                    System.out.println("Line is - " + strLine);
-                }
+    public void opera() {
+        System.setProperty("webdriver.opera.driver", "src\\browserpass\\operadriver.exe");
+        WebDriver driver = new OperaDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.linkedin.com/login");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.xpath("//button[text()='Sign in']"));
+        username.sendKeys("example@gmail.com");
+        password.sendKeys("password");
+        login.click();
+        String actualUrl = "https://www.linkedin.com/feed/";
+        String expectedUrl = driver.getCurrentUrl();
+        if (actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
 
-            }
+    public void firefox() {
+        System.setProperty("webdriver.gecko.driver", "src\\browserpass\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.linkedin.com/login");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.xpath("//button[text()='Sign in']"));
+        username.sendKeys("chepseron@gmail.com");
+        password.sendKeys("benson32!");
+        login.click();
+        String actualUrl = "https://www.linkedin.com/feed/";
+        String expectedUrl = driver.getCurrentUrl();
+        if (actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
+        }
+    }
+
+    public void IE() {
+        System.setProperty("webdriver.ie.driver", "src\\browserpass\\IEDriverServer.exe");
+        WebDriver driver = new InternetExplorerDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.linkedin.com/login");
+        WebElement username = driver.findElement(By.id("username"));
+        WebElement password = driver.findElement(By.id("password"));
+        WebElement login = driver.findElement(By.xpath("//button[text()='Sign in']"));
+        username.sendKeys("example@gmail.com");
+        password.sendKeys("password");
+        login.click();
+        String actualUrl = "https://www.linkedin.com/feed/";
+        String expectedUrl = driver.getCurrentUrl();
+        if (actualUrl.equalsIgnoreCase(expectedUrl)) {
+            System.out.println("Test passed");
+        } else {
+            System.out.println("Test failed");
         }
     }
 
